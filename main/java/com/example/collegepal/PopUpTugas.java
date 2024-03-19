0c126256e7e75e62bda64f220d8901518b884c59
Adrian Septiyadi <114846280+Adrs26@users.noreply.github.com>
2024-03-19 11:45:37 +0700

    Add files via upload

---
+++ main/java/com/example/collegepal/PopUpTugas.java
@@ -0,0 +1,14 @@
+package com.example.collegepal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PopUpTugas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup_tugas);
+    }
+}
